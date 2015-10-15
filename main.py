4224a13e0a0d6e16db85617d0d864863bb59e2f7
James <j1459.j1459@gmail.com>
2015-10-15 16:11:06 +0800

    Seems ready for proper testing on an actual asagi install.

--- main.py
+++ main.py
@@ -11,13 +11,16 @@
 import logging
 import re
 import os
+import sqlalchemy# Database library
 
 import config # Settings and configuration
 import lockfiles # MutEx lockfiles
 from utils import * # General utility functions
+import sql_functions# Database interaction
+from tables import Board# Table definitions
 
 
-def save_vocaroo(vocaroo_link,output_dir):
+def save_vocaroo_link(vocaroo_link,output_dir):
     """Take a vocaroo link and save it
     example input:
         http://vocaroo.com/i/s0xtktsit8rE
@@ -25,6 +28,7 @@ def save_vocaroo(vocaroo_link,output_dir):
     logging.debug("vocaroo_link: "+repr(vocaroo_link))
     logging.debug("output_dir: "+repr(output_dir))
 
+    # Grab the ID of the vocaroo
     vocaroo_id_search = re.search("""vocaroo.com/i/(\w+)""", vocaroo_link, re.IGNORECASE)
     vocaroo_id = vocaroo_id_search.group(1)
     logging.debug("vocaroo_id: "+repr(vocaroo_id))
@@ -55,22 +59,116 @@ def save_vocaroo(vocaroo_link,output_dir):
     return
 
 
-def find_vocaroo_links_in_db(session,start_id,stop_id):
+def find_vocaroo_links_in_db(session,start_id,stop_id,output_dir):
     """Scan through an asagi database and find vocaroo links.
     return a list of links"""
+    logging.info("Starting to process posts from the DB. ("+repr(start_id)+" to "+repr(stop_id)+")")
+    # Request the posts from the DB
+    posts_query = sqlalchemy.select([Board]).\
+        where(Board.doc_id >= start_id).\
+        where(Board.doc_id <= stop_id)
+    post_rows = session.execute(posts_query)
+
+    # Scan each post requested, downloading any found links
+    post_counter = 0
+    for post_row in post_rows:
+        post_counter += 1
+        logging.debug("post_counter: "+repr(post_counter))
+        comment = post_row["comment"]
+        logging.debug("comment: "+repr(comment))
+
+        # Find links
+        vocaroo_links = find_vocaroo_links_in_string(to_scan=comment)
+        number_of_vocaroo_links_found = len(vocaroo_links)
+        logging.debug("vocaroo_links: "+repr(vocaroo_links))
+
+        # Save links
+        for vocaroo_link in vocaroo_links:
+            save_vocaroo_link(
+                vocaroo_link,
+                output_dir,
+                )
+        continue
+    logging.info("Finished processing this batch of posts. ("+repr(start_id)+" to "+repr(stop_id)+")")
+    return number_of_vocaroo_links_found
+
+
+def scan_db(session,output_dir,start_id=0,stop_id=None,step_number=1000):
+    """Scan over a DB table of arbitrary size and process all rows"""
+    logging.info("Scanning DB...")
+    if stop_id is None:
+        # Find highest ID in DB and set stop_id to that
+        highest_id_query = sqlalchemy.select([Board]).\
+            order_by(sqlalchemy.desc(Board.doc_id)).\
+            limit(1)
+        highest_id_rows = session.execute(highest_id_query)
+        highest_id_row = highest_id_rows.fetchone()
+        highest_id_in_table = highest_id_row["doc_id"]
+        logging.info("highest_id_in_table: "+repr(highest_id_in_table))
+        stop_id = highest_id_in_table
+
+    # Setup id number values for initial group
+    low_id = start_id
+    high_id = start_id +step_number
+
+    total_number_of_vocaroo_links_found = 0
+    # Loop to process posts in batches to keep memory use lower
+    while low_id <= stop_id:
+        logging.debug("low_id: "+repr(low_id)+" , high_id:"+repr(high_id))
+        # Process this group of rows
+        number_of_vocaroo_links_found = find_vocaroo_links_in_db(
+            session,
+            start_id,
+            stop_id,
+            output_dir
+            )
+        total_number_of_vocaroo_links_found += number_of_vocaroo_links_found
+        # Increase ID numbers
+        low_id = high_id
+        high_id += step_number
+        continue
+
+    logging.info("total_number_of_vocaroo_links_found: "+repr(total_number_of_vocaroo_links_found))
+    logging.info("Finished scanning DB")
+    return total_number_of_vocaroo_links_found
+
+
 
 
 def find_vocaroo_links_in_string(to_scan):
     """Take a string, such as an archived post's comment,
      and find vocaroo links in it if there are any"""
+    # http://vocaroo.com/i/s0xtktsit8rE
+    # (?:https?://)?(?:www\.)?vocaroo.com/i/\w+
+    vocaroo_links = re.findall("""(?:https?://)?(?:www\.)?vocaroo.com/i/\w+""", to_scan, re.DOTALL)
+    logging.debug("vocaroo_links: "+repr(vocaroo_links))
+    return vocaroo_links
 
 
 def debug():
     """where stuff is called to debug and test"""
-    save_vocaroo(
-        vocaroo_link="http://vocaroo.com/i/s0xtktsit8rE",
+##    save_vocaroo_link(
+##        vocaroo_link="http://vocaroo.com/i/s0xtktsit8rE",
+##        output_dir=os.path.join("debug", "output"),
+##        )
+
+    session = sql_functions.connect_to_db()
+
+##    find_vocaroo_links_in_db(
+##        session=session,
+##        start_id=0,
+##        stop_id=1000,
+##        output_dir=os.path.join("debug", "output")
+##        )
+
+    scan_db(
+        session=session,
         output_dir=os.path.join("debug", "output"),
+        start_id=0,
+        stop_id=None,
+        step_number=1000
         )
+    return
 
 
 def main():
--- tables.py
+++ tables.py
@@ -12,6 +12,7 @@ import sqlalchemy# Database library
 from sqlalchemy.ext.declarative import declarative_base# Magic for ORM
 import sqlalchemy.dialects.postgresql # postgreSQL ORM (JSON, JSONB)
 
+import config # Settings and configuration
 from utils import * # General utility functions
 
 
@@ -39,160 +40,18 @@ from utils import * # General utility functions
 # SQLAlchemy table setup
 Base = declarative_base()
 
-class Boards(Base):
+class Board(Base):
     """Class that defines the boards table in the DB"""
-    __tablename__ = "boards"
+    __tablename__ = config.target_table_name
     # Columns
-    board_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)# Local primary key
-    board_shortname = sqlalchemy.Column(sqlalchemy.UnicodeText())# MUST be unique
-    board_name = sqlalchemy.Column(sqlalchemy.UnicodeText())
-    board_shortname = sqlalchemy.Column(sqlalchemy.UnicodeText())
-    api_url = sqlalchemy.Column(sqlalchemy.UnicodeText())
-    hidden = sqlalchemy.Column(sqlalchemy.Boolean())# Should this be hidden from users?
+    doc_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)# Local primary key
+    comment = sqlalchemy.Column(sqlalchemy.UnicodeText())
 
 
-
-class Threads(Base):
-    """Class that defines the threads table in the DB"""
-    __tablename__ = "threads"
-    # Columns
-    thread_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)# Local primary key
-    board_id = sqlalchemy.Column(sqlalchemy.BigInteger(), sqlalchemy.ForeignKey("boards.board_id"))# Foreign key#TODO
-    hidden = sqlalchemy.Column(sqlalchemy.Boolean())# Should this be hidden from users?
-    #
-    sticky = sqlalchemy.Column(sqlalchemy.Boolean())# was this thread a sticky?
-    locked = sqlalchemy.Column(sqlalchemy.Boolean())# was this thread locked?
-    #
-    thread_number = sqlalchemy.Column(sqlalchemy.BigInteger)# Number of the thread on the origin server
-    time_of_deletion = sqlalchemy.Column(sqlalchemy.BigInteger)# Unix time deletion was noticed
-    time_last_updated = sqlalchemy.Column(sqlalchemy.BigInteger)# Unix time we last updated this thread
-    time_las_bumped = sqlalchemy.Column(sqlalchemy.BigInteger)# Unix time last bump was noticed
-
-
-    number_of_replies = None#TODO
-
-
-class Posts(Base):
-    """Class that defines the posts table in the DB"""
-    __tablename__ = "posts"
-    # Columns
-    post_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)# Local primary key
-    thread_id = sqlalchemy.Column(sqlalchemy.BigInteger(), sqlalchemy.ForeignKey("threads.thread_id"))# Foreign key#TODO
-    hidden = sqlalchemy.Column(sqlalchemy.Boolean())# Should this be hidden from users?
-    #
-    time_of_deletion = sqlalchemy.Column(sqlalchemy.BigInteger)# Unix time post deletion was noticed
-    #
-    post_title = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-    post_text = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-    poster_name = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-    poster_tripcode = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-    poster_email = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-
-
-
-
-class media(Base):
-    """Class that defines the media table in the DB"""
-    __tablename__ = "media"
-    # Columns
-    media_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)# Local primary key
-    hidden = sqlalchemy.Column(sqlalchemy.Boolean())# Should this be hidden from users?
-    banned = False# unknown use
-    #
-    poster_filename = sqlalchemy.Column(sqlalchemy.UnicodeText())#Filename reported for origin
-    board_filename = sqlalchemy.Column(sqlalchemy.UnicodeText())# Filename on remote server
-    local_filename = sqlalchemy.Column(sqlalchemy.UnicodeText())# Filename in local storage
-    #
-    width = sqlalchemy.Column(sqlalchemy.BigInteger)# Width of full image
-    height = sqlalchemy.Column(sqlalchemy.BigInteger)# Height of full image
-    #
-    filesize_in_bytes = sqlalchemy.Column(sqlalchemy.BigInteger)#
-    md5_base64_hash = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-    sha512base16_hash = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-
-
-class thumbnails(Base):
-    """Class that defines the thumbnails table in the DB"""
-    __tablename__ = "thumbnails"
-    # Columns
-    #
-    thumbnail_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)# Local primary key
-    hidden = sqlalchemy.Column(sqlalchemy.Boolean())# Should this be hidden from users?
-    is_op_thumbnail = sqlalchemy.Column(sqlalchemy.Boolean())# TRUE: is op; FALSE: is reply
-    #
-    thumbnail_width = sqlalchemy.Column(sqlalchemy.BigInteger)# Width of thumbnail
-    thumbnail_height = sqlalchemy.Column(sqlalchemy.BigInteger)# Height of thumbnail
-    #
-    local_filename = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-    filesize_in_bytes = sqlalchemy.Column(sqlalchemy.BigInteger)#
-    md5_base64_hash = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-    sha512_hash = sqlalchemy.Column(sqlalchemy.UnicodeText())#
-
-
-class media_associations(Base):
-    """Class that defines the media-thumbnail-post association table in the DB"""
-    __tablename__ = "media_associations"
-    # Columns
-    association_id = sqlalchemy.Column(sqlalchemy.BigInteger(), primary_key=True)
-    post_id = sqlalchemy.Column(sqlalchemy.BigInteger(), sqlalchemy.ForeignKey("posts.post_id")) # Local post ID
-    media_id = sqlalchemy.Column(sqlalchemy.BigInteger(), sqlalchemy.ForeignKey("media.media_id")) # Local media ID
-    op_thumbnail_id = sqlalchemy.Column(sqlalchemy.BigInteger(), sqlalchemy.ForeignKey("thumbnails.thumbnail_id")) # Local thumbnail ID of OP thumbnail
-    reply_thumbnail_id = sqlalchemy.Column(sqlalchemy.BigInteger(), sqlalchemy.ForeignKey("thumbnails.thumbnail_id")) # Local thumbnail ID of reply thumbnail
-    spoilered = sqlalchemy.Column(sqlalchemy.Boolean())# IS the image spoilered in the post?
 # /SQLAlchemy table setup
 
 
 
-def create_example_db_sqllite():
-    """Provide a DB session
-    http://www.pythoncentral.io/introductory-tutorial-python-sqlalchemy/"""
-    logging.debug("Opening DB connection")
-    # add "echo=True" to see SQL being run
-    engine = sqlalchemy.create_engine("sqlite:///pysagi.sqllite", echo=True)
-    # Bind the engine to the metadata of the Base class so that the
-    # declaratives can be accessed through a DBSession instance
-    Base.metadata.bind = engine
-    Base.metadata.create_all(engine)
-
-    DBSession = sqlalchemy.orm.sessionmaker(bind=engine)
-    # A DBSession() instance establishes all conversations with the database
-    # and represents a "staging zone" for all the objects loaded into the
-    # database session object. Any change made against the objects in the
-    # session won't be persisted into the database until you call
-    # session.commit(). If you're not happy about the changes, you can
-    # revert all of them back to the last commit by calling
-    # session.rollback()
-    session = DBSession()
-    session.commit()
-
-    logging.debug("Example DB created")
-    return
-
-def create_example_db_postgres():
-    """Provide a DB session
-    http://www.pythoncentral.io/introductory-tutorial-python-sqlalchemy/"""
-    logging.debug("Opening DB connection")
-    # add "echo=True" to see SQL being run
-    # postgresql://username:password@host/database_name
-    engine = sqlalchemy.create_engine("postgresql://postgres:postgres@localhost/pysagi", echo=True)
-    # Bind the engine to the metadata of the Base class so that the
-    # declaratives can be accessed through a DBSession instance
-    Base.metadata.bind = engine
-    Base.metadata.create_all(engine)
-
-    DBSession = sqlalchemy.orm.sessionmaker(bind=engine)
-    # A DBSession() instance establishes all conversations with the database
-    # and represents a "staging zone" for all the objects loaded into the
-    # database session object. Any change made against the objects in the
-    # session won't be persisted into the database until you call
-    # session.commit(). If you're not happy about the changes, you can
-    # revert all of them back to the last commit by calling
-    # session.rollback()
-    session = DBSession()
-    session.commit()
-
-    logging.debug("Example DB created")
-    return
 
 
 def main():
